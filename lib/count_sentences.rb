a90eb94fa14bc655054bb69ce8b8507c88756b74
Jacob Feldman <jacobgfeld@gmail.com>
2018-11-06 21:35:47 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,10 @@ class String
   end
 
   def count_sentences
+<<<<<<< HEAD
     self.split(/\. |\? |\! /).length
+=======
+    self.split(/\.( |$)|\?( |$)|\!( |$)/).length
+>>>>>>> 1f7d6b700cc7e8392a8661e20928b744831ec748
   end
 end
